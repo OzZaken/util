2c6c9ca2947f9ed302d53e62fb957f344b98dd33
OzZaken <ozzaken0201@gmail.com>
2023-04-02 00:29:07 +0300

    utils

--- ORM/mongoose/controllers/product.controller.mongoose.ts
+++ ORM/mongoose/controllers/product.controller.mongoose.ts
@@ -8,7 +8,10 @@ const createProduct = async (product: IProduct): Promise<IProduct> => {
 const readProduct = async (id: string): Promise<IProduct | null> => {
     return await Product.findById(id).exec()
 }
-
+/**Partial<IProduct> creates a new type by making all properties of the IProduct interface optional.
+ *  This means that when we call the updateProduct function,
+ *  we can pass in an object with only some of the properties of the IProduct interface,
+ *  and the function will only update those properties. */
 const updateProduct = async (id: string, product: Partial<IProduct>): Promise<IProduct | null> => {
     return await Product.findByIdAndUpdate(id, product, { new: true }).exec()
 }
